9f30c07db5c63b36dc1e0907d306a788c0fd5a18
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-11 13:35:25 -0700

    Component ModalCreateTodo

--- src/components/ModalCreateTodo/index.tsx
+++ src/components/ModalCreateTodo/index.tsx
@@ -1,10 +1,50 @@
+import { useRef, FC } from 'react';
 import Styles from './styles.module.css';
 
-const ModalCreateTodo = () => {
-    
+interface Props {
+    modal: boolean;
+    setModal: (todo: boolean) => void;
+}
+
+const ModalCreateTodo: FC<Props> = ({modal, setModal}) => {
+
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    const generateTodo = () => {
+        
+        const textArea = textareaRef.current;
+
+        if (textArea) {
+            if (textArea.value !== '') {
+                
+                console.log(textArea.value);
+                setModal(false);
+            }
+        }
+    }
+
     return (
-        <section>
-            
+        <section className = {Styles['section']}>
+            <form className = {Styles['section__form']}>
+                <label
+                    className = {Styles['section__form--label']}
+                >
+                    Haz un nuevo TODO
+                </label>
+                
+                <textarea
+                    className = {Styles['section__form--textarea']}
+                    ref = {textareaRef}
+                ></textarea>
+                
+                <button
+                    className = {Styles['section__form--button']}
+                    type = 'button'
+                    onClick={() => generateTodo()}
+                >
+                    Crear TODO
+                </button>
+            </form>
         </section>
     );
 }
