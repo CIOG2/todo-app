47c571c1a69b04e229aa97e09b37bf65a12edf9f
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-11 16:59:41 -0700

    APP

--- src/APP/App.tsx
+++ src/APP/App.tsx
@@ -1,11 +1,43 @@
-import './App.css'
+import { useState, useEffect } from 'react'
+import { getFromLocalStorage, saveToLocalStorage } from '../utils/localStorage'
+import AddTodo from '../components/AddTodo'
+import ModalCreateTodo from '../components/ModalCreateTodo';
+import Todo from '../components/Todo'
+import Styles from './App.module.css'
 
 function App() {
+    const [ modal , setModal ] = useState(false);
+    const [ todoList, setTodoList ] = useState(getFromLocalStorage());
 
     return (
-        <div className="App">
-            <h1>React Typescript</h1>            
-        </div>
+        <main className = {Styles['main']}>
+            <AddTodo 
+                modal = {modal}
+                setModal = {setModal}
+            />
+            {modal && 
+                <ModalCreateTodo
+                    modal = {modal}
+                    setModal = {setModal}
+                    setTodoList = {setTodoList} 
+                />
+            }
+            <section className = {Styles['main__section']}>
+                {(todoList.length > 0) 
+                    ?(todoList.map((todo: any, index: number) => 
+                        <Todo
+                            key = {index}
+                            index = {index}
+                            text = {todo.text}
+                            completed = {todo.completed}
+                            setTodoList = {setTodoList}
+                        />
+                    ))
+                    :<p>No hay TODOs</p>  
+                }
+            </section>
+        </main>
+
     )
 }
 
