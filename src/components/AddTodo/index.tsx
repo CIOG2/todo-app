750800248b449d35f9c55dd7acc38fd6af891e5c
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-11 12:44:20 -0700

    Componente AddTodo

---
+++ src/components/AddTodo/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import Styles from './styles.module.css';
+
+interface Props {
+    modal: boolean;
+    setModal: (todo: boolean) => void;
+}
+
+const AddTodo: FC<Props> = ({modal, setModal}) => {
+    
+    const changeModalState = () => setModal(!modal);
+
+    return (
+        <button
+            className = {`
+                ${Styles['button']} 
+                ${modal ? Styles['close__modal'] : Styles['open__modal']}
+            `}
+            onClick = {() => changeModalState()}
+        >
+            +
+        </button>    
+    );
+}
+
+export default AddTodo;
