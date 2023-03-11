25bd9b7e42686890309b1e455203772ede76e50d
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-11 12:50:49 -0700

    Archivos para generar nuevos todos

---
+++ src/components/ModalCreateTodo/index.tsx
@@ -0,0 +1,12 @@
+import Styles from './styles.module.css';
+
+const ModalCreateTodo = () => {
+    
+    return (
+        <section>
+            
+        </section>
+    );
+}
+
+export default ModalCreateTodo;
