7fcb64e780aaf721ec73f441f4126c62d41954b7
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-14 20:16:56 -0700

    Archivos para el componente Options para los todos

---
+++ src/components/Todo/Options/index.tsx
@@ -0,0 +1,13 @@
+import Styles from './index.module.css';
+
+const Option = () => {
+    return (
+        <button
+            className={Styles['option']}
+        >
+        
+        </button>
+    )
+}
+
+export default Option;
