a9e10bff29ff0a37090e4a29dc2d783f98408276
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-22 00:11:44 -0700

    Nuevas funciones

--- src/utils/DOM.ts
+++ src/utils/DOM.ts
@@ -1,15 +1,30 @@
-const addDisplayNone = (index: number) => {
+const addDisplayNone = ( index: number ) => {
     const node = document.getElementById(`item-${index}`);
     
     if (node)
         node.classList.add('display-none');
 }
 
-const removeDisplayNone = (index: number) => {
+const removeDisplayNone = ( index: number ) => {
     const node = document.getElementById(`item-${index}`);
-
+    
     if (node)
         node.classList.remove('display-none');
 }
 
-export { addDisplayNone, removeDisplayNone};
+const addIsEditing = ( index: number ) => {
+    const node = document.getElementById(`article-todo-${index}`);
+
+    if (node)
+        node.classList.add('todo-edit');
+}
+
+const removeIsEditing = ( index: number ) => {
+    const node = document.getElementById(`article-todo-${index}`);
+
+    if (node)
+        node.classList.remove('todo-edit');
+}
+
+
+export { addDisplayNone, removeDisplayNone, addIsEditing, removeIsEditing};
