54bf06ce76c1f724a446ac8bcd20ca5f350bcc4c
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-23 15:44:52 -0700

    Se agregaron mas opciones de renderizado al Componente

--- src/components/Todo/Options/index.tsx
+++ src/components/Todo/Options/index.tsx
@@ -1,13 +1,44 @@
-import Styles from './index.module.css';
+import { useState } from 'react';
+import Buttons from '@components/Todo/Buttons';
+import Styles from '@components/Todo/Options/styles.module.css';
+import PropsButtons from '@interfaces/propsButtons';
 
-const Option = () => {
+interface Props extends PropsButtons {
+    setIsEdit: (state: boolean) => void;
+}
+
+const Options = (props: Props) => {
+
+    const [isOptionsOpen, setIsOptionsOpen] = useState(false);
+
+    const openOptions = () => {                
+        setIsOptionsOpen(!isOptionsOpen);
+    }
+
     return (
-        <button
-            className={Styles['option']}
-        >
-        
-        </button>
+        <div className = {Styles['container']}>
+            <button 
+                className = {Styles['container__button']}
+                style = {isOptionsOpen ? {zIndex: 15} : {zIndex: 0}}
+                onClick = { () => openOptions() }
+            >
+                {isOptionsOpen 
+                    ?<div className={ Styles["close"] }>X</div>
+                    :<div className={ Styles["points"] }></div>
+                }
+            </button>
+
+            {isOptionsOpen &&     
+                <>
+                    <div className={Styles['shadow']}></div>
+                    <Buttons 
+                        {...props} 
+                        setIsOptionsOpen = {setIsOptionsOpen}
+                    />
+                </>
+            }
+        </div>
     )
 }
 
-export default Option;
+export default Options;
