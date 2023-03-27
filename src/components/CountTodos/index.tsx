45bd7404d3bf47081e9f957411a8dbf9b431177b
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-26 20:58:11 -0700

    Componente Contador de todos

---
+++ src/components/CountTodos/index.tsx
@@ -0,0 +1,12 @@
+import Styles from '@components/CountTodos/styles.module.css';
+
+const CountTodos = () => {
+
+    return (
+        <section>
+            
+        </section>
+    )
+};
+
+export default CountTodos;
