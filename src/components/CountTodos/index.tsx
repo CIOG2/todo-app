931edcc75bc8567156c4347a15bd57c3ad0ec8f7
Carlos ivan <carlos.ivan10043@gmail.com>
2023-03-30 01:00:15 -0700

    Componente CountTodos Muestra un mensaje dependiendo de la cantidad de todos

--- src/components/CountTodos/index.tsx
+++ src/components/CountTodos/index.tsx
@@ -1,10 +1,33 @@
+import { FC } from 'react';
+import TodoI from '@interfaces/todo';
 import Styles from '@components/CountTodos/styles.module.css';
 
-const CountTodos = () => {
+interface Props {
+    todoList: TodoI[];
+}
+
+const CountTodos: FC<Props> = ({ todoList }) => {
+
+    const totalTodos = todoList.length;
+    const todoCompleted = todoList.filter(todo => todo.completed).length;
+
+    const mesaage = () => {
+        if (totalTodos === 0)
+            return `No hay nada por aqui`;
+        else if (todoCompleted === totalTodos)
+            return `Terminaste todos tus TODO'S`;
+        else
+            return `Llevas ${todoCompleted} todo's de ${totalTodos}`;
+    }
+    
 
     return (
-        <section>
-            
+        <section className = {Styles['section__title']}>
+            <h1
+                className = {Styles['section__title--count']}    
+            > 
+                {mesaage()}
+            </h1>
         </section>
     )
 };
